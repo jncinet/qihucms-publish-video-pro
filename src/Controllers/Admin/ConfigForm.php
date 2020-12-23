45781aaa80b4b93a8a9a78258f18089d46548b22
张晔 <y@qilephp.com>
2020-12-23 16:30:26 +0800

    update

--- src/Controllers/Admin/ConfigForm.php
+++ src/Controllers/Admin/ConfigForm.php
@@ -6,7 +6,7 @@ use App\Plugins\Plugin;
 use Encore\Admin\Widgets\Form;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
-use Illuminate\Support\Facades\Storage;
+use Qihucms\EditEnv\EditEnv;
 
 class ConfigForm extends Form
 {
@@ -37,19 +37,8 @@ class ConfigForm extends Form
 
         unset($data['publish-video-proLicenseKey']);
 
-        foreach ($data as $key => $value) {
-            if ($request->hasFile($key)) {
-                // 删除原文件
-                if (Cache::get($key) && Storage::exists(Cache::get($key))) {
-                    Storage::delete(Cache::get($key));
-                }
-                $file = $request->file($key);
-                Cache::put($key, Storage::putFile('config/publish-video', $file));
-            } else {
-                Cache::put($key, $value);
-            }
-        }
-
+        $env = new EditEnv();
+        $env->setEnv($data);
         admin_success($message);
 
         return back();
@@ -63,9 +52,9 @@ class ConfigForm extends Form
         $this->divider('授权');
         $this->text('publish-video-proLicenseKey', '插件授权')
             ->help('购买授权地址：<a href="http://ka.qihucms.com/product/" target="_blank">http://ka.qihucms.com</a>');
-        $this->number('ffmpeg_video_width', '视频宽度')->help('转码统一的视频宽度，');
-        $this->number('ffmpeg_video_height', '视频高度')->help('转码统一的视频高度');
-        $this->number('ffmpeg_input_duration', '视频时长限制')
+        $this->number('ff_video_width', '视频宽度')->help('转码统一的视频宽度，');
+        $this->number('ff_video_height', '视频高度')->help('转码统一的视频高度');
+        $this->number('ff_video_duration', '视频时长限制')
             ->help('限制视频发布时长，超出部份会被删除，为0时不限制，单位：秒，如果要动态修改，可以通过类的setInputDuration(15)方法修改');
     }
 
@@ -73,9 +62,9 @@ class ConfigForm extends Form
     {
         return [
             'publish-video-proLicenseKey' => Cache::store('file')->get('publish-video-proLicenseKey'),
-            'ffmpeg_video_width' => Cache::get('ffmpeg_video_width', 544),
-            'ffmpeg_video_height' => Cache::get('ffmpeg_video_height', 960),
-            'ffmpeg_input_duration' => Cache::get('ffmpeg_input_duration', 0),
+            'ff_video_width' => config('qihu.ff_video_width', 544),
+            'ff_video_height' => config('qihu.ff_video_height', 960),
+            'ff_video_duration' => config('qihu.ff_video_duration', 0),
         ];
     }
 }
